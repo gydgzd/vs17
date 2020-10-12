4f8415416e555a8284260e47a16450676a7241ab
gydgzd <gyd.001@163.com>
2020-10-12 20:35:22 +0800

    add test of Sleep in windows(Precision_Sleep)

--- ConsoleTest/example_boost/example_boost.cpp
+++ ConsoleTest/example_boost/example_boost.cpp
@@ -7,8 +7,34 @@
 #include <iostream>
 #include <iterator>
 //#include <algorithm>
+#include <chrono>
 
 extern void testMultiIndex();
+extern int testThread();
+
+void Precision_Sleep(int interval)
+{
+    using namespace std::chrono;
+    std::string buffer;
+    buffer.assign(256, 0);
+    buffer.clear();
+    int i = 10;
+    uint64_t total_used = 0;
+    while (i) {
+        i--;
+        steady_clock::time_point time_begin = steady_clock::now();
+        Sleep(interval);
+        steady_clock::time_point time_end = steady_clock::now();
+        char tmp[128] = { 0 };
+        uint64_t used = duration_cast<microseconds>(time_end - time_begin).count();
+        snprintf(tmp, 128, "%s Sleep %d ms, time used : %lld us\n",  __FUNCTION__, interval, used);
+        total_used += used;
+        buffer += tmp;
+    }
+    printf("%s\n", buffer.c_str());
+    printf("%s Sleep %d ms, average %lld us\n\n",  __FUNCTION__, interval, total_used / 10);
+
+}
 int main()
 {
 	using namespace boost::lambda;
@@ -16,6 +42,9 @@ int main()
 	std::cout << "Input some number please:" << std::endl;
 //	std::for_each(
 //		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+    Precision_Sleep(1);
+    Precision_Sleep(10);
+    testThread();
     testMultiIndex();
 
 
---
+++ ConsoleTest/example_boost/testThread.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <boost/thread.hpp>
+
+void wait(int seconds)
+{
+    boost::this_thread::sleep(boost::posix_time::seconds(seconds));
+}
+boost::mutex mutex;
+void thread()
+{
+    try {
+        for (int i = 0; i < 5; ++i)
+        {
+            wait(1);
+            mutex.lock();
+            std::cout << "tid:" << boost::this_thread::get_id() << " " << i << std::endl;
+            mutex.unlock();
+        }
+    }
+    catch (boost::thread_interrupted&)
+    {
+        std::cout << "Get an exception." << std::endl;
+    }
+
+}
+
+int testThread()
+{
+    boost::thread t(thread);
+    boost::thread t1(thread);
+    wait(3);
+    t.interrupt();
+    t.join();
+    t1.join();
+    return 0;
+}
