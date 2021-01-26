b96b6f2b6d8fa446f9c96b96be6bec625f345330
gydgzd <gyd.001@163.com>
2021-01-26 20:53:58 +0800

    测试boost::asio回调函数

--- ConsoleTest/example_boost/testAsio.cpp
+++ ConsoleTest/example_boost/testAsio.cpp
@@ -11,6 +11,78 @@ testAsio::~testAsio()
 {
 }
 
+// start --> async_connect --> on_connect
+void AsyncClient::start(ip::tcp::endpoint ep)
+{
+    std::cout << "connecting to "  << ep.address().to_string() << ":" << int(ep.port())<< std::endl;//  << " / " << ep.protocol()
+    sock_.async_connect(ep, MEM_FN2(on_connect, ep, _1));
+}
+
+AsyncClient::ptr AsyncClient::start(ip::tcp::endpoint ep, const std::string & message)
+{
+    ptr new_(new AsyncClient(message));
+    new_->start(ep);
+    return new_;
+}
+
+void AsyncClient::stop()
+{
+    if (!started_) return;
+    started_ = false;
+    sock_.close();
+}
+
+void AsyncClient::on_connect(ip::tcp::endpoint ep, const asio_error & err)
+{
+    if (!err.code())
+    {
+        std::cout << "connected to: " << ep.address().to_string() << ":" << int(ep.port()) << std::endl;
+        do_write(message_ + "\n");
+    }
+    else
+    {
+        std::cout << "on_connect error to " << ep.address().to_string() << ":" << int(ep.port()) << " - " << err.code() << " - " << err.what() << std::endl;
+        stop();
+    }
+}
+
+void AsyncClient::on_read(ip::tcp::socket &sock, const asio_error & err, size_t bytes)
+{
+    if (!err.code()) {
+        std::string copy(read_buffer_, bytes - 1);
+        std::cout << "server echoed our " << sock.remote_endpoint().address().to_string() << message_ << ": " << (copy == message_ ? "OK" : "FAIL") << std::endl;
+    }
+    else
+    {
+        std::cout << "on_read error: " << sock.remote_endpoint().address().to_string() << err.code() << " - " << err.what() << std::endl;
+        stop();
+    }
+   
+}
+
+void AsyncClient::on_write( const asio_error & err, size_t bytes)
+{
+    if (!err.code()) {
+        do_read(sock_);
+    }
+    else
+    {
+        std::cout << "on_write error: " << err.code() << " - " << err.what() << std::endl;
+        stop();
+    }
+}
+
+void AsyncClient::do_read(ip::tcp::socket &sock) {
+    async_read(sock_, buffer(read_buffer_), MEM_FN2(read_complete,  _1, _2), MEM_FN3(on_read, boost::ref(sock_), _1, _2));   // sock_   will be error
+}
+
+void AsyncClient::do_write(const std::string & msg) {
+    if (!started()) return;
+    std::copy(msg.begin(), msg.end(), write_buffer_);
+    sock_.async_write_some(buffer(write_buffer_, msg.size()), MEM_FN2(on_write, _1, _2));
+}
+
+
 void handler1(const boost::system::error_code &ec)
 {
 	for(int i = 0; i < 5; i++)
@@ -24,6 +96,7 @@ void handler2(const boost::system::error_code &ec)
 }
 
 boost::asio::io_service myservice;
+boost::asio::io_service service;
 
 void run()
 {
@@ -70,6 +143,10 @@ int testAsio::test()
         std::cout << "connect error: " << e.code() << " - " << e.what() << std::endl;
     }
     */
+
+    ip::tcp::endpoint ep(ip::address::from_string("10.1.4.169"), 8000);
+    auto shrd_client = AsyncClient::start(ep, "hi");
+    service.run();
 
     // server
     
--- ConsoleTest/example_boost/testAsio.h
+++ ConsoleTest/example_boost/testAsio.h
@@ -3,7 +3,7 @@
 #include <boost/asio.hpp> 
 #include <boost/thread.hpp> 
 #include <iostream>
-
+#include <string>
 using namespace boost::asio;
 
 class testAsio
@@ -16,3 +16,41 @@ public:
 	int test();
 };
 
+#define MEM_FN(x)       boost::bind(&self_type::x, shared_from_this())
+#define MEM_FN1(x,y)    boost::bind(&self_type::x, shared_from_this(),y)
+#define MEM_FN2(x,y,z)  boost::bind(&self_type::x, shared_from_this(),y,z)
+#define MEM_FN3(x,w,y,z)  boost::bind(&self_type::x, shared_from_this(),w,y,z)
+extern boost::asio::io_service service;
+
+class AsyncClient : public boost::enable_shared_from_this<AsyncClient>, boost::noncopyable
+{
+
+public:
+    typedef boost::system::system_error asio_error;
+    typedef boost::shared_ptr<AsyncClient> ptr;
+    static ptr start(ip::tcp::endpoint ep, const std::string &message);
+    void stop();
+    bool started() { return started_; }
+    void on_connect(ip::tcp::endpoint ep, const asio_error & err);
+
+    void on_read(ip::tcp::socket &sock, const asio_error & err, size_t bytes);
+    void on_write(  const asio_error & err, size_t bytes);
+    void do_read(ip::tcp::socket &sock);
+    void do_write(const std::string & msg);
+    size_t read_complete(  const boost::system::error_code & err, size_t bytes) {
+        // 和TCP客户端中的类似
+        std::cout << "read_complete" << std::endl;
+        return 0;
+    }
+private:
+    AsyncClient(const std::string & message) : sock_(service), started_(true), message_(message) {}
+    typedef AsyncClient self_type;
+    
+    void start(ip::tcp::endpoint ep);
+    ip::tcp::socket sock_;
+    enum { max_msg = 1024 };
+    char read_buffer_[max_msg];
+    char write_buffer_[max_msg];
+    bool started_;
+    std::string message_;
+};
