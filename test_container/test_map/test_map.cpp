5de3a1c7b8ce8d531f898ca72d9545b0dcc1739d
gyd <gyd.001@163.com>
2026-05-22 23:18:36 +0800

    update test

--- test_container/test_map/test_map.cpp
+++ test_container/test_map/test_map.cpp
@@ -13,9 +13,8 @@ protected:
         m_map.insert(std::pair<int, char>(2, 'a'));
         m_map.insert(std::pair<int, char>(3, 'b'));
     }
-
+    // map is ordered
     std::map<int, char> m_map;
-
 };
 
 TEST_F(MAP_TEST, test_initialize) {
@@ -23,27 +22,63 @@ TEST_F(MAP_TEST, test_initialize) {
     std::map<int, char> map_t{ {3,'a'}, {2,'b'}, {4, 'e'}};
     EXPECT_EQ(map_t.size(), 3);
 
-    // visit by iter
+	// visit by iterator, map is ordered by key
     std::map<int, char>::iterator iter = map_t.begin();
-
-    // map is ordered
     EXPECT_EQ(iter->first, 2);
     EXPECT_EQ(iter->second, 'b');
 }
 
 TEST_F(MAP_TEST, test_add) {
-    m_map.insert({ 11, 'a' });
-    m_map.insert({ 13, 'z' });
+    m_map.insert({11, 'a'});
+    m_map.insert({13, 'z'});
     EXPECT_EQ(m_map.size(), 5);
 
-    m_map.insert({ 13, 'c' });
+	m_map.insert({ 13, 'c' });      // not wording, because key 13 already exists
     EXPECT_EQ(m_map.size(), 5);
 
     m_map.emplace(5, ' ');
     EXPECT_EQ(m_map[5], ' ');
     EXPECT_EQ(m_map.size(), 6);
 
-    m_map.emplace(3, ' ');
+	m_map.emplace(3, ' ');         // not wording, because key 3 already exists
     EXPECT_EQ(m_map[3], 'b');
     EXPECT_EQ(m_map.size(), 6);
+
+    // remove
+	m_map.erase(5);
+    EXPECT_EQ(m_map.find(5), m_map.end());
+    EXPECT_EQ(m_map.size(), 5);
+}
+
+TEST_F(MAP_TEST, test_modify) {
+    EXPECT_EQ(m_map[2], 'a');
+    for (auto& member : m_map) {
+        if (member.first == 2) {
+            member.second = 'x';
+        }
+    }
+    EXPECT_EQ(m_map[2], 'x');
+
+    // visit by iterator
+    std::map<int, char>::iterator iter = m_map.begin();
+    for (iter = m_map.begin(); iter != m_map.end(); ) {
+        if (iter->first == 1) {
+			iter = m_map.erase(iter);
+        } else {
+            ++iter;
+        }
+    }
+    EXPECT_EQ(m_map.find(1), m_map.end());
+}
+
+TEST_F(MAP_TEST, test_bound) {
+	// lower_bound: returns an iterator to the first element not less than the given key
+	auto iter1 = m_map.lower_bound(1);
+    EXPECT_EQ(iter1->first, 1);
+    EXPECT_EQ(iter1->second, '0');
+
+	// upper_bound: returns an iterator to the first element greater than the given key
+    auto iter2 = m_map.upper_bound(1);
+    EXPECT_EQ(iter2->first, 2);
+    EXPECT_EQ(iter2->second, 'a');
 }
--- test_container/test_vector/test_vector.cpp
+++ test_container/test_vector/test_vector.cpp
@@ -1,20 +1,155 @@
-﻿// test_vector.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING  
 #include <iostream>
+#include <time.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <vector>
+#include <algorithm>   // for sort
+#include <numeric>     // for accumulate
+#include <execution>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(VECTOR_TEST, test_initialize) {
+    // initialize
+    std::vector<int> vec_int{ 1, 2, 3, 4, 5 };
+    EXPECT_EQ(vec_int.size(), 5);
+
+    // visit by at()
+    for (int i = 0; i < 5; i++)
+        EXPECT_EQ(vec_int.at(i), i + 1);
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+TEST(VECTOR_TEST, test_visit) {
+    // initialize
+    std::vector<int> vec_int{ 1, 2, 3, 4, 5 };
+    clock_t start, end;
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+    // visit by at()
+    start = clock();
+    for (int i = 0; i < vec_int.size(); i++) {
+        vec_int.at(i) = i;
+    }
+    end = clock();
+    cout << "visit by at() cost " << (double)(end - start) / CLOCKS_PER_SEC << " s" << endl;
+    // vec_int.at(1001) = 0;   //  "invalid vector<T, N> subscript"
+
+    // visit by [] (result shows that [] can be faster than at())
+    start = clock();
+    for (int i = 0; i < vec_int.size(); i++) {
+        vec_int[i] = i;
+    }
+
+    end = clock();
+    cout << "visit by [] cost " << (double)(end - start) / CLOCKS_PER_SEC << " s" << endl;
+    // vec_int[1001] = 0;      // vector subscript out of range
+
+    // visit by iterator
+    int i = 0;
+    start = clock();
+    for (auto iter = vec_int.begin(); iter != vec_int.end(); iter++) {
+        *iter = i++;
+    }
+
+    end = clock();
+    cout << "visit by iterator cost " << (double)(end - start) / CLOCKS_PER_SEC << " s" << endl;
+}
+
+TEST(VECTOR_TEST, test_insert) {
+    // initialize
+    std::vector<int> vec_int{ 1, 2, 3};
+    EXPECT_EQ(vec_int.size(), 3);
+    EXPECT_EQ(vec_int.capacity(), 3);
+
+	vec_int.insert(vec_int.end(), 4);
+	vec_int.push_back(5);
+	vec_int.emplace_back(6);
+    EXPECT_EQ(vec_int.size(), 6);
+    EXPECT_EQ(vec_int.capacity(), 6);
+
+	vec_int.reserve(10);
+    EXPECT_EQ(vec_int.capacity(), 10);
+}
+
+TEST(VECTOR_TEST, test_remove) {
+    // initialize
+    std::vector<int> vec_int{ 1, 2, 3, 4 };
+    EXPECT_EQ(vec_int.size(), 4);
+    EXPECT_EQ(vec_int.capacity(), 4);
+
+    vec_int.erase(vec_int.begin());
+	EXPECT_EQ(vec_int.size(), 3);
+    EXPECT_EQ(vec_int.capacity(), 4);
+
+    // [first, last)
+    vec_int.erase(vec_int.begin(), vec_int.begin()+2);
+    EXPECT_EQ(vec_int.size(), 1);
+    EXPECT_EQ(vec_int.capacity(), 4);
+}
+
+TEST(VECTOR_TEST, test_modify) {
+    // initialize
+    std::vector<int> vec_int{ 1, 2, 3, 4, 5 };
+    std::fill(vec_int.begin(), vec_int.end(), 12);
+    EXPECT_EQ(vec_int[0], 12);
+
+    for (int i = 0; i < vec_int.size(); i++) {
+        EXPECT_EQ(vec_int[i], 12);
+    }
+    int* p = vec_int.data();
+    *p = 5;
+    EXPECT_EQ(vec_int[0], 5);
+}
+
+TEST(VECTOR_TEST, test_size) {
+    // initialize
+    std::vector<int> vec_int;
+    EXPECT_EQ(vec_int.size(), 0);
+    EXPECT_EQ(vec_int.empty(), true);
+}
+
+TEST(VECTOR_TEST, test_pair_vector) {
+    //
+    std::vector<std::pair<int, int>> arr_pair{ std::make_pair(1, 0), std::make_pair(-1, 0), std::make_pair(0, 1), std::make_pair(0, -1) };
+    // assign(): change every element to one value
+	std::fill(arr_pair.begin(), arr_pair.end(), std::make_pair(2, 2));
+
+    // data() : return the pointer of the first element
+    std::pair<int, int>* px = arr_pair.data();
+    cout << "visit by data:" << px->first << endl;
+
+    cout << "vector front:" << arr_pair.front().first << endl;
+}
+
+TEST(VECTOR_TEST, test_function) {
+    //
+    std::vector<int> vec_int{ 1, 4, 2, 5 };
+    cout << "before sort:" << vec_int[0] << " " << vec_int[1] << " " << vec_int[2] << " " << vec_int[3] << endl;
+
+    // sort   std::execution is supported after C++17,
+#if (defined(__cplusplus) && __cplusplus >= 201703L) 
+    || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
+        std::sort(std::execution::seq, vec_int.begin(), vec_int.end());
+#  else
+    std::sort(vec_int.begin(), vec_int.end());
+#  endif
+
+    cout << "after  sort:" << vec_int[0] << " " << vec_int[1] << " " << vec_int[2] << " " << vec_int[3] << endl;
+
+    EXPECT_EQ(vec_int[0], 1);
+    EXPECT_EQ(vec_int[1], 2);
+    EXPECT_EQ(vec_int[2], 4);
+    EXPECT_EQ(vec_int[3], 5);
+
+    // find
+    std::vector<int> vec_int1{ 1, 4, 2, 5 };
+    auto found = find(vec_int1.begin(), vec_int1.end(), 2);
+    EXPECT_EQ(*found, 2);
+
+    auto found1 = find(vec_int1.begin(), vec_int1.end(), 6);
+    EXPECT_EQ(found1, vec_int1.end());
+
+    // accumulate: calculate the sum of all elements
+    int sum = accumulate(vec_int1.begin(), vec_int1.end(), 0);
+    EXPECT_EQ(sum, 12);
+}
